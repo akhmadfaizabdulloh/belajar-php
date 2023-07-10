7ed0d9b473f5dff886782259a495fb1ee7fb8eb5
Akhmad Faiz Abdulloh <akhmadfaizabdulloh@gmail.com>
2023-07-10 23:48:21 +0700

    8-while.php

---
+++ 8-while.php
@@ -0,0 +1,21 @@
+<!--  
+While berguna untuk membuat sebuah perulangan
+yang tidak di ketahui berapa kali perulangan
+tersebut di lakukan. seperti pada saat kita
+ingin menampilkan data dari database.
+-->
+
+<!-- 
+
+while(kondisi){
+  Statement.
+}
+
+-->
+<?php
+$x = 1;
+while($x <= 10){
+  echo "Angka $x <br>";
+  $x++;
+}
+?>
